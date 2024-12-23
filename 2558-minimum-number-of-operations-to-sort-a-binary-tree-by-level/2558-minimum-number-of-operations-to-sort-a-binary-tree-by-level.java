7b1fc98d3eb74816a476aac77365278a106b8951
T3RM1N470R <de.arkaprab@gmail.com>
2024-12-23 20:20:56 +0530

    Time: 52 ms (69.18%), Space: 58.9 MB (98.63%) - LeetHub

---
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution
+{
+    public int minimumOperations(TreeNode root)
+    {
+        if(root == null)
+            return 0;
+        List<TreeNode> q = new ArrayList<>();
+        int c = 0;
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            List<TreeNode> q1 = new ArrayList<>();
+            for(int i = 0; i < q.size(); i++)
+            {
+                TreeNode p = q.get(i);
+                if(p.left != null)
+                    q1.add(p.left);
+                if(p.right != null)
+                    q1.add(p.right);
+            }
+            q = q1;
+            c += count(q);
+        }
+        return c;
+    }
+
+    int count(List<TreeNode> l)
+    {
+        int n = l.size(), a[] = new int[n], p[][] = new int[n][2], c = 0;
+        for(int i = 0; i < n; i++)
+            a[i] = l.get(i).val;
+
+        for(int i = 0; i < n; i++)
+        {
+            p[i][0] = a[i];
+            p[i][1] = i;
+        }
+
+        Arrays.sort(p, (x, y) -> x[0] - y[0]);
+        boolean v[] = new boolean[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            if(v[i] || i == p[i][1])
+                continue;
+            int s = 0, x = i;
+            while(!v[x])
+            {
+                v[x] = true;
+                x = p[x][1];
+                s++;
+            }
+            if(s > 1)
+                c += s - 1;
+        }
+        return c;
+    }
+}
