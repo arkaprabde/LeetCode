d1626bab38e5812031bdec6bccd3bb1b8640d36a
T3RM1N470R <de.arkaprab@gmail.com>
2024-12-23 19:19:23 +0530

    Time: 218 ms (100%), Space: 61.5 MB (100%) - LeetHub

---
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxDistinctElements(int[] a, int k)
+    {
+        TreeMap<Integer, Integer> mp = new TreeMap<>();
+        for(int x: a)
+            mp.put(x, mp.getOrDefault(x, 0) + 1);
+        int c = 0, y = -k, d;
+        for(int x: mp.keySet())
+        {
+            int s = mp.get(x);
+            if(y == -k)
+                d = k + 1;
+            else
+                d = Math.min(k+1, x - y);
+            if(s <= d + k)
+            {
+                c += s;
+                y = x - d + s;
+            }
+            else
+            {
+                c += d + k;
+                y = x + k;
+            }
+        }
+        return c;
+    }
+}
