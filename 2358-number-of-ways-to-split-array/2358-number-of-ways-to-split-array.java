280bcee36b062bf2696dc3adf8294f906f844727
T3RM1N470R <de.arkaprab@gmail.com>
2025-01-03 10:52:00 +0530

    Time: 3 ms (59.69%), Space: 57.6 MB (88.51%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
@@ -9,7 +9,7 @@ class Solution
         for(int i = 0; i < n - 1; i++)
         {
             v += a[i];
-            if((v << 1) >= s)
+            if(v << 1 >= s)
                 c++;
         }
         return c;
