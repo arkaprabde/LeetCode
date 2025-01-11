ca2325e7279d60da24aad7cdbc1b9cab81130900
T3RM1N470R <de.arkaprab@gmail.com>
2025-01-11 18:47:18 +0530

    Time: 4 ms (94.6%), Space: 45.5 MB (45.32%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -4,6 +4,8 @@ class Solution
     {
         if(s.length() < k)
             return false;
+        if(s.length() == k)
+            return true;
         int a[] = new int[26], v = 0;
         for(char c: s.toCharArray())
             a[c - 'a']++;
