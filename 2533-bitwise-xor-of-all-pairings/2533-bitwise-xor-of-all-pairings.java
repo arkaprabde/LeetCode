ca3282f0a719e867a2629032d16b7bc895f6dbe2
T3RM1N470R <de.arkaprab@gmail.com>
2025-01-16 12:27:38 +0530

    Time: 1 ms (100%), Space: 61 MB (80.17%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.java
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.java
@@ -2,15 +2,7 @@ class Solution
 {
     public int xorAllNums(int[] n1, int[] n2)
     {
-        int x = n1.length & 1, y = n2.length & 1;
-        if(x == 1 && y == 1)
-            return xor(n1) ^ xor(n2);
-        else if(x == 1)
-            return xor(n2);
-        else if(y == 1)
-            return xor(n1);
-        else
-            return 0;
+        return (xor(n1) * (n2.length & 1)) ^ (xor(n2) * (n1.length & 1));
     }
 
     int xor(int a[])
