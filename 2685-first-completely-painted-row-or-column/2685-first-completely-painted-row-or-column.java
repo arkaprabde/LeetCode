bde6ccfdcd46555058c7ccb8d9b721d027cf710f
T3RM1N470R <de.arkaprab@gmail.com>
2025-01-20 12:01:53 +0530

    Time: 3 ms (99.5%), Space: 57.6 MB (100%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
@@ -2,24 +2,23 @@ class Solution
 {
     public int firstCompleteIndex(int[] a, int[][] p)
     {
-        Map<Integer, int[]> mp = new HashMap<>();
-        Map<Integer, List<Integer>> smp1 = new HashMap<>(), smp2 = new HashMap<>();
-        int n = p.length, m = p[0].length;
+        int n = p.length, m = p[0].length, b[] = new int[n * m + 1], s = n * m + 1;
+        for(int i = 0; i < n * m; i++)
+            b[a[i]] = i;
         for(int i = 0; i < n; i++)
-            for(int j = 0; j < m; j++)
-                mp.put(p[i][j], new int[]{i, j});
-        for(int i = 0; i < n; i++)
-            smp1.put(i, new ArrayList<>());
-        for(int i = 0; i < m; i++)
-            smp2.put(i, new ArrayList<>());
-        for(int i = 0; i < a.length; i++)
         {
-            int b[] = mp.get(a[i]);
-            smp1.get(b[0]).add(b[1]);
-            smp2.get(b[1]).add(b[0]);
-            if(smp1.get(b[0]).size() == m || smp2.get(b[1]).size() == n)
-                return i;
+            int mx = 0;
+            for(int j = 0; j < m; j++)
+                mx = Math.max(mx, b[p[i][j]]);
+            s = Math.min(s, mx);
         }
-        return -1;
+        for(int i = 0; i < m; i++)
+        {
+            int mx = 0;
+            for(int j = 0; j < n; j++)
+                mx = Math.max(mx, b[p[j][i]]);
+            s = Math.min(s, mx);
+        }
+        return s;
     }
 }
