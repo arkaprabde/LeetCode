1a642794749efa6e5596698edf0f2abd229f3c45
T3RM1N470R <de.arkaprab@gmail.com>
2025-01-23 18:44:23 +0530

    Time: 3 ms (47.43%), Space: 52.5 MB (58.31%) - LeetHub

---
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public int countServers(int[][] a)
+    {
+        int n = a.length, m = a[0].length, s1[] = new int[n], s2[] = new int[m];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+            {
+                s1[i] += a[i][j];
+                s2[j] += a[i][j];
+            }
+        int c = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                if(a[i][j] == 1 && (s1[i] > 1 || s2[j] > 1))
+                    c++;
+        return c;
+    }
+}
