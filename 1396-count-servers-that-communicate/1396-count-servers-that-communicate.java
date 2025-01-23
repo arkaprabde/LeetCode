ec841c493cc2a10cd423118adbabeb96337dd90b
T3RM1N470R <de.arkaprab@gmail.com>
2025-01-23 18:45:23 +0530

    Time: 2 ms (100%), Space: 52.7 MB (38.37%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
@@ -5,10 +5,11 @@ class Solution
         int n = a.length, m = a[0].length, s1[] = new int[n], s2[] = new int[m];
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++)
-            {
-                s1[i] += a[i][j];
-                s2[j] += a[i][j];
-            }
+                if(a[i][j] == 1)
+                {
+                    s1[i]++;
+                    s2[j]++;
+                }
         int c = 0;
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++)
