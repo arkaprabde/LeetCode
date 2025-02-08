468fec13f8a6b554b50732f157cf3487384396cc
T3RM1N470R <de.arkaprab@gmail.com>
2025-02-08 18:55:49 +0530

    Time: 56 ms (96.58%), Space: 130.4 MB (7.53%) - LeetHub

---
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class NumberContainers
+{
+    private Map<Integer, PriorityQueue<Integer>> mp1;
+    private Map<Integer, Integer> mp2;
+    public NumberContainers()
+    {
+        mp1 = new HashMap<>();
+        mp2 = new HashMap<>();
+    }
+    public void change(int index, int number)
+    {
+        if (mp2.containsKey(index))
+        {
+            int x = mp2.get(index);
+            if (x == number)
+                return;
+            mp1.get(x).remove(index);
+        }
+        mp1.computeIfAbsent(number, k -> new PriorityQueue<>()).offer(index);
+        mp2.put(index, number);
+    }
+    
+    public int find(int number)
+    {
+        PriorityQueue<Integer> pq = mp1.getOrDefault(number, new PriorityQueue<>());
+        return pq.isEmpty() ? -1 : pq.peek();
+    }
+}
