14375d6b93d4a06fb1731c69dc036def0d762f28
T3RM1N470R <de.arkaprab@gmail.com>
2025-02-09 17:52:50 +0530

    Time: 28 ms (99.71%), Space: 57.6 MB (61.76%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
@@ -5,11 +5,7 @@ class Solution
         Map<Integer, Long> mp = new HashMap<>();
         long s = 0, n = a.length;
         for(int i = 0; i < a.length; i++)
-        {
-            a[i] -= i;
-            mp.put(a[i], mp.getOrDefault(a[i], 0L) + 1);
-            s += mp.get(a[i]) - 1;
-        }
+            s += mp.merge(a[i] - i, 1L, Long::sum) - 1;
         return n * (n - 1) / 2 - s;
     }
 }
